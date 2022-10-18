d1731f16f4ee5cc01fc2f8a1a94eabe0e920e214
yeongrong-heo <102986339+yeongr1203@users.noreply.github.com>
2022-10-18 15:55:29 +0900

    Delete JS2 directory

--- JS2/ch13/change-color.js
+++
@@ -1,9 +0,0 @@
-
-var heading = document.querySelector('#heading'); 
-heading.onclick = function() {
-    heading.style.color ='red';
-}
-
-function changeColor(target) {  
-    target.style.color = 'red';
-} 
